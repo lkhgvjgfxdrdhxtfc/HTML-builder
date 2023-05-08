5d21dad2c0d98717eff0c82dbe94a5846f0b50f6
lkhgvjgfxdrdhxtfc <wrk_ml@mail.ru>
2023-05-08 19:29:38 +0300

    feat: add code in index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,61 @@
+const fs = require('fs');
+const path = require("path");
+var fs_Extra = require('fs-extra');
+
+var sourceDir = path.join(__dirname, "../04-copy-directory/files");
+var destinationDir = path.join(__dirname, "../04-copy-directory/files-copy");
+
+/*
+if (!fs.existsSync(destinationDir)){
+    fs.mkdirSync(destinationDir, { recursive: true });
+}
+*/
+/*
+fs_Extra.remove(destinationDir, err => {
+    console.error(err)
+  })
+*/
+/*
+if (fs.access(destinationDir)) {
+    console.log('ýes');
+}
+*/
+/*
+fs.access(destinationDir, err => {
+if (err) {
+//console.log(false);
+return false;
+} else {
+
+//    console.log(true);
+    return true;
+}
+ 
+});
+*/
+
+
+
+/*
+fs_Extra.remove(destinationDir, err => {
+    console.error(err);
+  });
+
+*/
+/*
+    fs_Extra.remove(destinationDir, err => {
+        console.error(err);
+      });
+*/
+
+fs_Extra.copy(sourceDir, destinationDir, function(error) {
+    if (error) {
+        throw error;
+    } else {
+      console.log("success!");
+    }
+});
+
+
+
 
